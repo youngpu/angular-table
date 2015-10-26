d72ee11180ca5ed1100b2037c42ed8ce9916ecbf
mkfreeman <michael.k.freeman@gmail.com>
2015-10-25 18:02:06 -0700

    Prepared activity for class

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 // Create an angular application called "sortApp"
-var sortApp = angular.module('sortApp', [])
 
-// Define a controller "mainController" for you application
-sortApp.controller('mainController', function($scope, $http) {
+
+// Define a controller "mainController" for you application, 
+// making sure to pass in both the $scope and $http variables
+
 	// Use and $http.get request to get the salary data
-	$http.get('data/salaries.json').then(function(results) {
+	
 		// Set $scope.data equal to the data returned in your results
-		$scope.data = results.data;
-	})
-});
